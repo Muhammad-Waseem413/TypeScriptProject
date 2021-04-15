cdcd2aa50d034dc582ee7a5563df34b48874e2b5
Waseem <saim788@gmail.com>
2021-04-15 17:46:52 +0500

    adding a todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,50 @@
+import React from "react";
 import "./App.css";
 
+type formElement = React.FormEvent<HTMLFormElement>;
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
+
 function App() {
-  const sum = (a: number, b: number): number => {
-    return a + b;
+  const [value, setValue] = React.useState<string>("");
+  const [todos, setTodo] = React.useState<ITodo[]>([]);
+
+  const handleSubmit = (event: formElement): void => {
+    event.preventDefault();
+    addTodo(value);
+    setValue("");
   };
+
+  const addTodo = (text: string) => {
+    const newTodo: ITodo[] = [...todos, { text, complete: false }];
+    setTodo(newTodo);
+  };
+
+  console.log(todos);
 
   return (
     <div className="App">
-      <h3> Welcome to the react typescript</h3>
-      {sum(2, 4)}
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+          required
+        ></input>
+        <button type="submit">Add Todo</button>
+      </form>
+      <div>
+        {todos.map((todo: ITodo, index: number) => {
+          return (
+            <ul key={index}>
+              <li>{todo.text}</li>
+            </ul>
+          );
+        })}
+      </div>
     </div>
   );
 }
